4fdf96e0be5793a8786326b8fe92bd88ff060b04
Quentin Wood <quentin.wood14159@gmail.com>
2026-09-16 21:47:15 -0600

    FirstCommit

---
+++ TrailHeadSurvey/src/App.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import Card from "./Cards.tsx";
+
+type Animal = {
+  name: string;
+  image: string;
+  count: number;
+}
+
+function App() {
+const [animals, setAnimals] = useState<Animal[]>([
+  {
+    name: "Mountain Lion",
+    image: "/images/mountain-lion.jpg",
+    count: 0,
+  },
+  {
+    name: "Black Bear",
+    image: "/images/black-bear.jpg",
+    count: 0,
+  },
+  {
+    name: "Elk",
+    image: "/images/elk.jpg",
+    count: 0,
+  },
+  {
+    name: "Bighorn Sheep",
+    image: "/images/bighorn-sheep.jpg",
+    count: 0,
+  }, 
+]);
+
+function addAnimal(index: number) {
+    const updatedAnimals = [...animals];
+
+    updatedAnimals[index].count += 1;
+
+    setAnimals(updatedAnimals);
+  }
+
+  function subtractAnimal(index: number) {
+    const updatedAnimals = [...animals];
+
+    if (updatedAnimals[index].count > 0) {
+      updatedAnimals[index].count -= 1;
+    }
+
+    setAnimals(updatedAnimals);
+  }
+
+  function submitSurvey() {
+    console.log(animals);
+  }
+
+
+  return (
+    <><div className="card-container">
+      {animals.map((animal, index) => (
+        <Card
+          key={animal.name}
+          name={animal.name}
+          image={animal.image}
+          count={animal.count}
+          onAdd={() => addAnimal(index)}
+          onSubtract={() => subtractAnimal(index)} />
+      ))}
+    </div><><button onClick={submitSurvey}>Submit</button></></>
+    
+
+  );
+}
+
+export default App;
---
+++ TrailHeadSurvey/src/Cards.tsx
@@ -0,0 +1,33 @@
+
+
+type CardProps = {
+  name: string;
+  image: string;
+  count: number;
+  onAdd: () => void;
+  onSubtract: () => void;
+};
+
+function Card({ name, image, count, onAdd, onSubtract }: CardProps) {
+  
+
+  return (
+    <div className="card">
+      <img src={image} alt={name} />
+
+      <h1>{name}</h1>
+
+      <div className="counter">
+        <button onClick={onSubtract}>-</button>
+
+        <h2>{count}</h2>
+
+        <button onClick={onAdd}>+</button>
+      </div>
+    </div>
+  );
+}
+
+
+
+export default Card;
